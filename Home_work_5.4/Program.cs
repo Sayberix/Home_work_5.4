22200a29a0e4279fed30eac31b53240a2f1632ec
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-06 22:52:15 +0700

    Начало работы с дз 5.4.2

--- Home_work_5.4/Program.cs
+++ Home_work_5.4/Program.cs
@@ -6,30 +6,11 @@ namespace Home_work_5._4
     {
         static void Main(string[] args)
         {
-            // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-            // 3, 5-> 243(3⁵)
-            // 2, 4-> 16
+            // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+            // 452-> 11
+            // 82-> 10
+            // 9012-> 12
 
-            int[] array = new int[2];
-            enterArray();                        
-            Console.WriteLine($"Число: {array[0]} в степени: {array[1]} равно: {Math.Pow(array[0], array[1])}");
-
-            void enterArray()
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    if (i == 0)
-                    {
-                        Console.WriteLine("Введите значение, которое будет возводится в степень: ");
-                        array[i] = Convert.ToInt32(Console.ReadLine());
-                    }
-                    if (i == 1)
-                    {
-                        Console.WriteLine("Введите степень, в которую возведется предыдущее введеное значение: ");
-                        array[i] = Convert.ToInt32(Console.ReadLine());
-                    }
-                }
-            }
         }
     }
 }
