805c8b71920749d9c65ee4f71f6c2a188d955851
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-07 20:29:44 +0700

    исправил вручную отступы

--- Home_work_5.4/Program.cs
+++ Home_work_5.4/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Home_work_5._4
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
             //1, 2, 5, 7, 19-> [1, 2, 5, 7, 19]
@@ -14,9 +14,9 @@ namespace Home_work_5._4
             fillArray(array);           // Функция заполнения массива случайными числами
             showArray(array);           // Функция выводит элементы массива в цикле
 
-            void fillArray (int [] array)
+            void fillArray(int[] array)
             {
-                for(int i = 0; i < array.Length; i++)
+                for (int i = 0; i < array.Length; i++)
                 {
                     array[i] = new Random().Next(1, 100);
                 }
@@ -27,7 +27,7 @@ namespace Home_work_5._4
                 Console.Write("Массив состоит из следующих элементов: ");
                 for (int i = 0; i < array.Length; i++)
                 {
-                    if(i == 0)
+                    if (i == 0)
                         Console.Write("[" + array[i] + ", ");
                     else
                         if (i == array.Length - 1)
