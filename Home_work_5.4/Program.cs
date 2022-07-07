aae8a27be06b7e2538aa1f4946895fcf526ac1a8
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-07 18:17:01 +0700

    дз 5.4.3 выполнено!

--- Home_work_5.4/Program.cs
+++ Home_work_5.4/Program.cs
@@ -10,7 +10,32 @@ namespace Home_work_5._4
             //1, 2, 5, 7, 19-> [1, 2, 5, 7, 19]
             //6, 1, 33-> [6, 1, 33]
 
-            
+            int[] array = new int[8];
+            fillArray(array);
+            showArray(array);
+
+            void fillArray (int [] array)
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    array[i] = new Random().Next(1, 100);
+                }
+            }
+
+            void showArray(int[] array)
+            {
+                Console.Write("Массив состоит из следующих элементов: ");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if(i == 0)
+                        Console.Write("[" + array[i] + ", ");
+                    else
+                        if (i == array.Length - 1)
+                            Console.Write(array[i] + "]");
+                        else
+                            Console.Write(array[i] + ", ");
+                }
+            }
         }
     }
 }
